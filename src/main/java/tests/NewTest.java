a784b57a522adac85a4abe5e2bc53a6fd9cb749e
singareddy <singareddy46@gmail.com>
2020-03-17 11:14:30 +0000

    adding

---
+++ src/main/java/tests/NewTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	public WebDriver driver;
+	
+  @Test
+  public void openMyBlog() {
+	  System.out.println("************************************I am in openMyBlog method...");
+	driver.get("https://www.softwaretestingmaterial.com/");
+  }
+  
+  @BeforeClass
+  public void beforeClass() {
+	  
+	  System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\Drivers\\geckodriver.exe");
+	  driver = new FirefoxDriver();
+	  
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
